8fed6f5580ed39ab2e63572513afde6a51c10935
Lakshya Khandelwal <123928987+Lakshya0304@users.noreply.github.com>
2024-09-28 20:44:31 +0000

    comparisions

---
+++ 01_basics/04_comparision.js
@@ -0,0 +1,13 @@
+console.log("2" > 1);
+
+console.log( null > 0);     //false
+console.log( null == 0);    //false
+console.log( null >= 0);    //true
+
+//Reason is that the equality check ==  and comparision > < >= <= work differently
+//comaparision convert null to a number and treat as 0
+
+//strict check 
+// ===
+
+console.log(2 === "2");
