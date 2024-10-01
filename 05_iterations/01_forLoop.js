78994544d67ec9e50a1817230128011c135520a9
Lakshya Khandelwal <123928987+Lakshya0304@users.noreply.github.com>
2024-10-01 20:47:45 +0000

    loop

---
+++ 05_iterations/01_forLoop.js
@@ -0,0 +1,26 @@
+//For loop
+
+for (let index = 0; index < 10; index++) {
+    const element = index;
+    // console.log(element);
+}
+
+// for (let i = 1; i <= 10; i++) {
+//     // console.log(`Outer loop value : ${i}`);
+//     console.log(`Table of ${i} is :`);
+//     for (let j = 1; j <= 10; j++) {
+//         // console.log(`Inner loop value : ${j} and with outer loop value ${i}`);
+//         console.log( i + '*' + j + '=' + i*j);
+//     } 
+//     console.log(`---------------------------------------------------------------`);
+// }
+
+
+
+
+// const myArray = ['flash' , 'batman' , 'superman']
+// for (let index = 0; index < myArray.length; index++) {
+//     const element = myArray[index];
+//     console.log(element); 
+// }
+
---
+++ 05_iterations/02_whileLoop.js
@@ -0,0 +1,33 @@
+//while 
+
+// while (condition) {
+    
+// }
+
+// let i = 0
+// while (i<=10) {
+//     console.log(`${i}`);
+//     i = i+2
+// }
+
+// let myArray = ['flash' , 'batman' , 'superman']
+// let arr = 0
+// while (arr < myArray.length){
+//     console.log(`${myArray[arr]}`);
+//     arr++;
+// }
+
+
+
+
+
+//do-while
+
+// do {
+    
+// } while (condition);
+let score = 1
+do {
+    console.log(`${score}`); 
+    score++;   
+} while (score <=10);
